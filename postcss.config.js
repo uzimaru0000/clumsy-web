7057393392bb5a53f3dbecaa43f39812f9582aaf
uzimaru0000 <shuji365630@gmail.com>
2020-09-13 03:09:34 +0900

    :construction_worker: Add CI setting

--- postcss.config.js
+++ postcss.config.js
@@ -5,21 +5,19 @@ const plugins = [tailwind];
 if (process.env.NODE_ENV !== 'development') {
   const purgecss = require('@fullhuman/postcss-purgecss');
 
-  class TailwindExtractor {
-    static extract(content) {
-      return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-    }
-  }
-
   plugins.push(
     purgecss({
       content: ['src/*.html', 'src/**/*.tsx'],
-      extractors: [
-        {
-          extractor: TailwindExtractor,
-          extensions: ['html', 'tsx'],
-        },
-      ],
+      defaultExtractor: (content) => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches =
+          content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+        return broadMatches.concat(innerMatches);
+      },
     })
   );
 }
